a7bb09ff611c49b39f71ae7709100c8cd1c46716
John Adelakun <johnadelakun1@gmail.com>
2022-08-08 11:59:24 +0200

    added wordle

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import { ToastContainer } from "react-toastify";
+
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
+import Wordle from "./components/Wordle";
+
+function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        // random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
+  return (
+    <div className="App">
+      <NavBar />
+      <ToastContainer />
+      <Banner />
+      <Skills />
+      {solution && <Wordle solution={solution} />}
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Wordle.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
+
+// components
+import Grid from "./Grid";
+import Keypad from "./Keypad";
+
+export default function Wordle({ solution }) {
+  const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } =
+    useWordle(solution);
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
+
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup]);
+
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect);
+  }, [guesses, turn, isCorrect]);
+
+  return (
+    <section className="wordle" id="wordle">
+      <h1>Wordle (AfricaN3)</h1>
+      <div>solution - {solution}</div>
+      <div>Current Guess - {currentGuess}</div>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad usedKeys={usedKeys} />
+    </section>
+  );
+}
