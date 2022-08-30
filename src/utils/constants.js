da4bf8356b23ba7c3a5018a73ca851d21a14e4bf
John Adelakun <johnadelakun1@gmail.com>
2022-08-30 13:37:22 +0200

    changed testnet to mainnet contract constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,9 @@ export const gasContractAddress = "0xd2a4cff31913016155e38e474a2c06d08be276cf";
 export const magpieContractAddress =
   "0x5bc5af5418efb1f07b7fa0c42107f2722c75af50";
 
-export const nodeUrl = "http://seed1t5.neo.org:20332"; //"https://mainnet5.neo.coz.io:443"
+export const mainnetNodeUrl = "http://seed1t5.neo.org:20332"; //"https://mainnet5.neo.coz.io:443"
 
-export const mainnetNodeUrl = "https://mainnet5.neo.coz.io:443";
+export const nodeUrl = "https://mainnet5.neo.coz.io:443";
 
 export const factor = Math.pow(10, 8);
 
